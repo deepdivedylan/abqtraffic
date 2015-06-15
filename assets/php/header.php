d1c1823442a2ee72b0c55eac1de88e79395415d4
Dylan McDonald <dmcdonald21@cnm.edu>
2015-06-15 14:44:58 -0600

    Added base libraries and blank templates

---
+++ assets/php/header.php
@@ -0,0 +1,26 @@
+<?php
+require_once(dirname(dirname(__DIR__)) . "/root-path.php");
+/**
+* Get the relative path
+*
+* @see https://raw.githubusercontent.com/kingscreations/farm-to-you/master/php/lib/header.php FarmToYou Header
+**/
+$CURRENT_DEPTH = substr_count($CURRENT_DIR, "/");
+$ROOT_DEPTH = substr_count($ROOT_PATH, "/");
+$DEPTH_DIFFERENCE = $CURRENT_DEPTH - $ROOT_DEPTH;
+$PREFIX = str_repeat("../", $DEPTH_DIFFERENCE);
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet" />
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+		<link type="text/css" href="<?php echo $PREFIX; ?>assets/css/abqtraffic.css" rel="stylesheet" />
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
+		<title><?php echo $PAGE_TITLE; ?></title>
+	</head>
+	<body>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+$CURRENT_DIR = __DIR__;
+$PAGE_TITLE = "ABQ Traffic";
+require_once("assets/php/header.php");
+require_once("assets/php/footer.php");

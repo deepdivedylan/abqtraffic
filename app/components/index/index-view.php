f86b15c4c7c4f655968452bee1007715daecf64b
Dylan McDonald <dmcdonald21@cnm.edu>
2015-06-16 10:31:03 -0600

    Changed the index view to show a table

--- app/components/index/index-view.php
+++ app/components/index/index-view.php
@@ -1,8 +1,13 @@
 <main class="container">
 	<h2>Current Barricades</h2>
-	<div class="row">
-		<div ng-repeat="barricade in barricades">
-			{{ barricade.attributes.OBJECTID }}
-		</div>
-	</div>
+	<table class="table table-bordered table-condensed table-hover table-striped">
+		<tr>
+			<th>Description</th><th>Start Time</th><th>End Date</th>
+		</tr>
+		<tr ng-repeat="barricade in barricades">
+			<td>{{ barricade.attributes.LOCATION_DESC }}</td>
+			<td>{{ barricade.attributes.STARTWORK | date: "medium" }}</td>
+			<td>{{ barricade.attributes.ENDWORK | date: "medium" }}</td>
+		</tr>
+	</table>
 </main>

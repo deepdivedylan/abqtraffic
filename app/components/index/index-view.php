ac7bb1c569d6b1dce3e464dba2e181bab678d166
Dylan McDonald <dmcdonald21@cnm.edu>
2015-06-17 16:56:16 -0600

    Cleaned up forms and added verify route front end

--- app/components/index/index-view.php
+++ app/components/index/index-view.php
@@ -1,7 +1,50 @@
 <main class="container">
+	<h2>Verify Your Route</h2>
+	<form id="verifyRoute" class="form-horizontal">
+		<input type="hidden" id="gpsOrigin-latitude" name="gpsOrigin-latitude" />
+		<input type="hidden" id="gpsOrigin-longitude" name="gpsOrigin-longitude" />
+		<input type="hidden" id="gpsDestination-latitude" name="gpsDestination-latitude" />
+		<input type="hidden" id="gpsDestination-longitude" name="gpsDestination-longitude" />
+		<section class="form-group">
+			<div class="col-md-6">
+				<label for="origin">Origin</label>
+				<div class="input-group">
+					<div class="input-group-addon">
+						<i class="fa fa-map-marker"></i>
+					</div>
+					<input type="text" name="origin" id="origin" placeholder="Enter Origin&hellip;" maxlength="128" />
+				</div>
+				<div class="input-group">
+					<input type="checkbox" id="gpsOrigin" name="gpsOrigin" />
+					<label for="gpsOrigin" class="control-label">Use Current Location</label>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<label for="destination">Destination</label>
+				<div class="input-group">
+					<div class="input-group-addon">
+						<i class="fa fa-map-marker"></i>
+					</div>
+					<input type="text" name="destination" id="destination" placeholder="Enter Destination&hellip;" maxlength="128" />
+				</div>
+				<div class="input-group">
+					<input type="checkbox" id="gpsDestination" name="gpsDestination" />
+					<label for="gpsDestination" class="control-label">Use Current Location</label>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="input-group">
+					<button type="submit" class="btn btn-lg btn-info">Verify</button>
+					<button type="reset" class="btn btn-lg btn-warning">Clear</button>
+				</div>
+			</div>
+		</section>
+	</form>
+	<section id="verifyStatus"></section>
+	<hr />
 	<h2>Current Barricades</h2>
-	<form>
-		<section class="form-group row">
+	<form class="form-horizontal">
+		<section class="form-group">
 			<div class="col-md-6">
 				<div class="input-group">
 					<input type="checkbox" id="detourOnly" name="detourOnly" ng-model="detourOnly" ng-click="detourFilter();" />
--- app/components/shared/filters.js
+++ app/components/shared/filters.js
@@ -1,7 +1,7 @@
 angular.module("abqtrafficFilters", ["ngSanitize"]).filter("checkmark", function($sce) {
 	return(function(input) {
-		var checkmark = $sce.trustAsHtml("<i class=\"glyphicon glyphicon-ok\"></i>");
-		var xmark = $sce.trustAsHtml("<i class=\"glyphicon glyphicon-remove\"></i>");
+		var checkmark = $sce.trustAsHtml("<i class=\"fa fa-check\"></i>");
+		var xmark = $sce.trustAsHtml("<i class=\"fa fa-times\"></i>");
 		return(input ? checkmark : xmark);
 	});
 });
